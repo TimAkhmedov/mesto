4b2b005729abfb3491329152c71c42a14253cc4d
Timur Akhmedov <tim.akhmedoff@gmail.com>
2022-03-20 02:39:10 +0300

    создать функционал добавления и лайка карточек

--- script/index.js
+++ script/index.js
@@ -4,16 +4,56 @@ let popup = container.querySelector('.popup');
 let openedPopup = container.querySelector('.popup_opened');
 let popupCloseBtns = container.querySelectorAll('.popup__close-btn');
 let popupAddCard = container.querySelector('.popup-card');
-/*let submitBtn = popup.querySelector('.popup__submit-btn');*/
-
+let popupAddCardForm = container.querySelector('.popup__card-add');
 let nameInput = container.querySelector('.popup__name-field');
 let jobInput = container.querySelector('.popup__job-field');
 let profileName = container.querySelector('.profile__name');
 let profileJob = container.querySelector('.profile__job');
 let popupProfileEdit = popup.querySelector('.popup__profile-edit');
-let likeBtn = container.querySelector('.card__like-btn');
-let likeBtns = container.querySelectorAll('.card__like-btn');
+/*let likeBtn = container.querySelector('.card__like-btn');
+let likeBtns = document.querySelectorAll('.card__like-btn')*/;
 let addCardBtn = container.querySelector('.profile__add-btn');
+let cardTitleInput = container.querySelector('.popup__card-title-field');
+let cardUrlInput = container.querySelector('.popup__card-url-field');
+const cardsList = document.querySelector('.cards');
+const cardTemplate = document.querySelector('.template-card').content;
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach(function (element) {
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.card__image').alt = element.name;
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__title').textContent = element.name;
+  cardElement.querySelector('.card__like-btn').addEventListener('click', (item) => {
+    item.target.classList.toggle('card__like-btn_active');
+  });
+  cardsList.append(cardElement);
+});
 
 function openPopup() {
   nameInput.value = profileName.textContent;
@@ -23,7 +63,6 @@ function openPopup() {
 
 function closePopup() {
   let openedPopup = container.querySelector('.popup_opened');
-  console.log(openedPopup);
   openedPopup.classList.remove('popup_opened');
 }
 
@@ -34,17 +73,30 @@ function addProfilePersonalData(evt) {
   closePopup();
 }
 
-function addCard() {
+function openPopupCard() {
   popupAddCard.classList.add('popup_opened');
 }
+
+function addNewCard (evt) {
+  evt.preventDefault();
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.card__image').src = cardUrlInput.value;
+  cardElement.querySelector('.card__image').alt = cardTitleInput.value;
+  cardElement.querySelector('.card__title').textContent = cardTitleInput.value;
+  cardElement.querySelector('.card__like-btn').addEventListener('click', (item) => {
+    item.target.classList.toggle('card__like-btn_active');
+  });
+  console.log(cardElement.querySelector('.card__like-btn'));
+  cardsList.prepend(cardElement);
+  closePopup();
+}
+
 editBtn.addEventListener('click', openPopup);
 popupCloseBtns.forEach(item => item.addEventListener('click', closePopup));
 popupProfileEdit.addEventListener('submit', addProfilePersonalData);
-/*submitBtn.addEventListener('submit', addProfilePersonalData);*/
-
-
-likeBtns.forEach(item => item.addEventListener('click', (item) => {
+popupAddCardForm.addEventListener('submit', addNewCard);
+addCardBtn.addEventListener('click', openPopupCard);
+/*likeBtns.forEach(item => item.addEventListener('click', (item) => {
+  console.log(item);
   item.target.classList.toggle('card__like-btn_active');
-}));
-
-addCardBtn.addEventListener('click', addCard);
+}));*/
